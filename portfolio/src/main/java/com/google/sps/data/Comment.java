fb2ad52d5111bedc835422f97b24ec30b5dc0107
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-06-04 20:40:58 -0400

    Update Comment.java

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -6,7 +6,13 @@ public final class Comment {
   private final long id;
   private final String text;
   private final long timestamp;
-
+  
+  /**
+   * This constructor uses the Datastore ID belonging
+   * to the comment as the id field, the comment's 
+   * actual text in its text field, and the comment's 
+   * creation time in milliseconds in its timestamp field.
+   */ 
   public Comment(long id, String text, long timestamp) {
     this.id = id;
     this.text = text;
