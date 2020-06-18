f4b44455d5fbab1b311fb9476f274e9130b2c707
Tomas <talvarez0507@gmail.com>
2020-06-18 20:14:22 +0000

    fix indentation

--- walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -14,13 +14,9 @@
 
 package com.google.sps;
 
-/**
- * Utility class for creating greeting messages.
- */
+/** Utility class for creating greeting messages. */
 public class Greeter {
-  /**
-   * Returns a greeting for the given name.
-   */
+  /** Returns a greeting for the given name. */
   public String greet(String name) {
     return "Hello " + name.trim();
   }
